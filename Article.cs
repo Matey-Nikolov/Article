79113c2d1a133522a4ffde8fb04ba950d1763a2a
Matey-Nikolov <matey.nikolov@mail.bg>
2021-03-07 18:28:22 +0200

    Add project files.

---
+++ Article.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articles
+{
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Rename(string rename)
+        {
+            Title = rename;
+        }
+
+        public void Edit(string edit)
+        {
+            Content = edit;
+        }
+
+        public void ChangeAuthor(string changeAuthor)
+        {
+            Author = changeAuthor;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Articles
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string[] commandssomeOrNo = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string rename = commandssomeOrNo[0];
+            string edit = commandssomeOrNo[1];
+            string changeAuthor = commandssomeOrNo[2];
+
+            Article article = new Article(rename, edit, changeAuthor);
+            
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(": ", StringSplitOptions.RemoveEmptyEntries);
+
+                string renameeEditChangeAuthor1 = commands[0];
+                string message = commands[1];
+
+                if (renameeEditChangeAuthor1 == "Edit")
+                {
+                    article.Edit(message);
+                }
+                else if (renameeEditChangeAuthor1 == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(message);
+                }
+                else if (renameeEditChangeAuthor1 == "Rename")
+                {
+                    article.Rename(message);
+                }
+            }
+
+            Console.WriteLine(article.ToString());
+           // Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+
+        }
+    }
+}
